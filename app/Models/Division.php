ed0e08c92d6942b678dba4ff61c83015575c2f78
Delion <21adtydwf@gmail.com>
2025-11-24 10:15:57 +0700

    feat(update): menentukan kolom yang bisa diisi, menentukan kolom yang dicasting, dan menentukan relasi ke tabel user di model mahasiswa profile

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Division extends Model
 {
+    use HasFactory;
     protected $table = 'divisions';
 
     // pk id default, tp eksplisit aja
--- app/Models/MahasiswaProfile.php
+++ app/Models/MahasiswaProfile.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MahasiswaProfile extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'mahasiswa_profiles';
+
+    // bisa diisi
+    protected $fillable = 
+    [
+        'user_id',
+        'nim',
+        'program_studi',
+        'angkatan',
+        'ipk',
+        'semester',
+        'foto',
+        'cv_path',
+        'transkrip_path'
+    ];
+
+    // casting
+    protected $casts = 
+    [
+        $ipk => 'float',
+        $semester => 'integer',
+        $created_at => 'datetime',
+        $updated_at => 'datetime'
+    ];
+
+    // relasi
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
 }
