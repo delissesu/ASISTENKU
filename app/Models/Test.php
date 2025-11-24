bc8f09829e7d3cc0cfa955989abbf4b24a40bb1e
Delion <21adtydwf@gmail.com>
2025-11-24 12:18:18 +0700

    feat(update): menambah kolom fillable, menambah casting kolom, menambah relasi ke tabel lowongan, mahasiswa, test, dan hasil tes, serta menambah function helper di model application

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'tests';
+
+    protected $fillable = [
+
+    ];
+
+    public function application() {
+        return $this->belongsTo(Test::class, 'application_id');
+    }
 }
