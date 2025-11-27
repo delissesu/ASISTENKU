67e71c1389d1448296c092d9f983222416bb68cd
Delion <21adtydwf@gmail.com>
2025-11-27 16:08:26 +0700

    feat(update): memperbarui warna dan desain UI di bagian simpan perubahan

--- resources/views/components/student/profile-tab.blade.php
+++ resources/views/components/student/profile-tab.blade.php
@@ -102,7 +102,7 @@
                 </div>
 
                 <div x-show="isEditing" x-transition class="flex justify-end pt-2">
-                    <x-ui.button type="submit" class="bg-blue-600 hover:bg-blue-700 w-full md:w-auto">
+                    <x-ui.button type="submit" class="bg-blue-600 text-white hover:bg-blue-700 w-full md:w-auto">
                         <!-- Ikon Simpen -->
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4 mr-2"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/></svg>
                         Simpan Perubahan
