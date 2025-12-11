75dda6b03dbc02845deb1989f0fedd523be68aa9
Delion <21adtydwf@gmail.com>
2025-12-11 07:21:11 +0700

    feat(fix): update x-cloak agar mengikuti active tab, menunggu alpine selesai

--- resources/views/pages/recruiter/dashboard.blade.php
+++ resources/views/pages/recruiter/dashboard.blade.php
@@ -3,19 +3,19 @@
 @section('page-title', 'Dashboard')
 
 @section('content')
-    <div x-show="activeTab === 'overview'">
+    <div x-show="activeTab === 'overview'" @if($activeTab !== 'overview') x-cloak @endif>
         <x-recruiter.overview-tab :stats="$stats" :division-stats="$divisionStats" :recent-activity="$recentActivity" />
     </div>
-    <div x-show="activeTab === 'jobs'" x-cloak>
+    <div x-show="activeTab === 'jobs'" @if($activeTab !== 'jobs') x-cloak @endif>
         <x-recruiter.jobs-tab :jobs="$jobs" :job-stats="$jobStats" :divisions="$divisions" />
     </div>
-    <div x-show="activeTab === 'applicants'" x-cloak>
+    <div x-show="activeTab === 'applicants'" @if($activeTab !== 'applicants') x-cloak @endif>
         <x-recruiter.applicants-tab :applicants="$applicants" :applicant-stats="$applicantStats" :divisions="$divisions" />
     </div>
-    <div x-show="activeTab === 'exams'" x-cloak>
+    <div x-show="activeTab === 'exams'" @if($activeTab !== 'exams') x-cloak @endif>
         <x-recruiter.exams-tab :exams="$exams" :divisions="$divisions" />
     </div>
-    <div x-show="activeTab === 'announcements'" x-cloak>
+    <div x-show="activeTab === 'announcements'" @if($activeTab !== 'announcements') x-cloak @endif>
         <x-recruiter.announcements-tab :announcements="$announcements" :applicants="$applicants" />
     </div>
 
